3f8353063899c22445aac42649c8662d7d2b1f4b
Martin Romario Ntuwa <72998544+martinromario55@users.noreply.github.com>
2022-06-20 20:39:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+// Submission mode:
+console.log('Name: Martin Ntuwa')
+console.log('Height: 5"10"')
+console.log('Country: Kenya')
